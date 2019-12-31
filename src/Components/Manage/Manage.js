de9a81b79a8c1cc60fac702a54755ccba3956ac1
Elmi Elmi <elmielmi522@gmail.com>
2019-12-31 01:48:44 -0600

    added inputs to manage genres

--- src/Components/Manage/Manage.js
+++ src/Components/Manage/Manage.js
@@ -3,9 +3,33 @@ import React, { Component } from 'react';
 class Manage extends Component {
     render() {
         return (
-            <div>
-                <h1>Add Genre</h1>
-            </div>
+            <form>
+                <div>
+                    <h1>Add Genre</h1>
+                </div>
+                <div>
+                    <input type="text" placeholder="Project name"></input>
+                    <button>Submit</button>
+                    <br></br>
+                    <h2>Projects</h2>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Project</th>
+                                <th>Total Movies</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Scifi</td>
+                                <td>2</td>
+                                <td>Delete</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </form>
         );
     }
 }
